c7da333fb8e54c2ba6104ad9475bf529bfa6ea49
roze28 <firoze.hossain01@yahoo.com>
2022-02-19 19:23:10 +0600

    work with abstract

---
+++ JAVA/Abstract/Bike.java
@@ -0,0 +1,15 @@
+package com.roze.oop;
+
+/**
+ *
+ * @author firoze
+ */
+public abstract class Bike {
+
+    abstract void run();
+   
+    public void bikeModel(){
+        System.out.println("Bike Model= 5300H");
+    }
+
+}
---
+++ JAVA/Abstract/TestAbstract.java
@@ -0,0 +1,21 @@
+package com.roze.oop;
+
+/**
+ *
+ * @author firoze
+ */
+class TestAbstract extends Bike {
+
+    @Override
+    void run() {
+        System.out.println("Running safely");
+    }
+
+    public static void main(String[] args) {
+        Bike b = new TestAbstract();
+        b.run();
+        b.bikeModel();
+
+    }
+
+}
