f8930694e14368cf8897d33988937079a3360bce
roze28 <firoze.hossain01@yahoo.com>
2022-02-23 12:17:36 +0600

    Abstraction with Interface

---
+++ JAVA/OOP/src/main/java/com/roze/oop/abstraction/withInterface/TestWithInterface.java
@@ -0,0 +1,16 @@
+package com.roze.oop.abstraction.withInterface;
+
+/**
+ *
+ * @author firoze
+ */
+public class TestWithInterface {
+
+    public static void main(String[] args) {
+        A a = new M();
+        a.a();
+        a.b();
+        a.c();
+        a.d();
+    }
+}
