c039d9c9ae213c1588d171c330056149f0beffaf
roze28 <firoze.hossain01@yahoo.com>
2022-02-23 12:21:23 +0600

    abstraction with Interface

---
+++ JAVA/OOP/src/main/java/com/roze/oop/abstraction/withInterface/B.java
@@ -0,0 +1,14 @@
+
+package com.roze.oop.abstraction.withInterface;
+
+/**
+ *
+ * @author firoze
+ */
+public abstract class B implements A{
+    public void c(){
+        System.out.println("I am c");
+    
+    }
+    
+}
