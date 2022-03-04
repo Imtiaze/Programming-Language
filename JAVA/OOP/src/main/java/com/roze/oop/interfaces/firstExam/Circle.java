ab33d6f76403badce2182e094d014c30e71ace8d
roze28 <firoze.hossain01@yahoo.com>
2022-03-04 16:10:09 +0600

    first example of interface

---
+++ JAVA/OOP/src/main/java/com/roze/oop/interfaces/firstExam/Circle.java
@@ -0,0 +1,12 @@
+package com.roze.oop.interfaces.firstExam;
+
+/**
+ *
+ * @author firoze
+ */
+public class Circle implements Drawable {
+
+    public void draw() {
+        System.out.println("Drawing circle");
+    }
+}
---
+++ JAVA/OOP/src/main/java/com/roze/oop/interfaces/firstExam/TestDrawable.java
@@ -0,0 +1,19 @@
+package com.roze.oop.interfaces.firstExam;
+
+/**
+ *
+ * @author firoze
+ */
+public class TestDrawable {
+
+    public static void main(String[] args) {
+        Drawable da;
+        da = new Rectangle();
+        da.draw();
+        
+        da=new Circle();
+        da.draw();
+
+    }
+
+}
